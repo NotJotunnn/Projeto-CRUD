30cc744b12c60a87c8f267a9625723d3648da9a7
João Gabriel <jota.amaralmiranda@gmail.com>
2024-03-13 18:55:16 -0300

    Mudanças João Gabriel

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,23 @@
 function App() {
+
+    // Backend - React
+    function botao() {
+
+      console.log('Apertou')
+
+
+    }
+
+
   return (
     <>
-      <h1 className="text-sky-400 text-4xl font-semibold">Esta é nossa área de trabalho oficial</h1>
+      <h1 className="text-sky-200 text-5xl font-semibold">Esta é nossa área de trabalho oficial</h1>
+
+      <button onClick={botao}>Clique aqui</button>
+
     </>
+
+
   )
 }
 
