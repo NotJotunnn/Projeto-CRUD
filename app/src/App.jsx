5424031312a525ab161a09bb540aeffc76e018e0
João Gabriel <jota.amaralmiranda@gmail.com>
2024-03-14 22:14:56 -0300

    Add task cards in Front-End

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import CardsGroup from "./components/CardsGroup"
 import TaskForm from "./components/TaskForm"
 
@@ -5,6 +6,10 @@ function App() {
 
   // TODO: Adicionar estado para guardar os dados das tarefas
   // TODO: Passar estado dos dados das tarefas para <CardsGroup />
+
+  const [task, setTask] = useState([{"id":4,"task":"cleanupAvenue"},{"id":5,"task":"cleanupAvenue"},{"id":"182ea6b9-19c0-4a42-b435-55c6ce501eb5","task":"takeTheGarbageOut"}]);
+
+  //const props = {task, setTask};
 
 
   // TODO: Adicionar função para alterar os dados das tarefas
@@ -15,7 +20,7 @@ function App() {
     <div className="flex flex-col gap-5 items-center w-full min-h-screen bg-[#eee]">
       <h1 className="text-[#222] text-center text-7xl font-semibold mt-[42px]">Tarefas!</h1>
       <TaskForm />
-      <CardsGroup />
+      <CardsGroup task={task}/>
     </div>
   )
 }
--- app/src/components/Card.jsx
+++ app/src/components/Card.jsx
@@ -1,13 +1,25 @@
-const CardsGroup = () => {
+const Cards = ({props}) => {
+  
+  const {task, id} = props // desconstruindo array
+
+
+  function clickButton() { 
+
+    // Function DELETE task
+
+    console.log('Botão Pressionado', id)
+    
+  }
+  
   return (
     <div className="flex flex-row bg-[#777] w-[550px] h-[100px] rounded-[5px] justify-between overflow-hidden border-[3px] border-[#333]">
-      <h2 className="grow ml-[30px] my-auto text-2xl font-semibold text-white">A tarefa!</h2>
+      <h2 className="grow ml-[30px] my-auto text-2xl font-semibold text-white">{task}</h2>
       <div className="flex flex-col border-l-[3px] border-[#333]">
-        <button className="w-[52px] h-[50%] bg-[#4cff38] cursor-pointer border-b-[1.5px] border-[#333] hover:bg-[#278419]">✅</button>
+        <button onClick={ () => clickButton()} className="w-[52px] h-[50%] bg-[#4cff38] cursor-pointer border-b-[1.5px] border-[#333] hover:bg-[#278419]">✅</button>
         <button className="w-[52px] h-[50%] bg-[#ffe53b] cursor-pointer border-t-[1.5px] border-[#333] hover:bg-[#a89726]">🔄</button>
       </div>
     </div>
   );
 }
- 
-export default CardsGroup;
+
+export default Cards;
