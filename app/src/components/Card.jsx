0f35a2a17dd39f583e72bfe70b6e53a387ec94b2
João Gabriel <jota.amaralmiranda@gmail.com>
2024-03-15 17:41:48 -0300

    Function button created (Not Fixed)

--- app/src/components/Card.jsx
+++ app/src/components/Card.jsx
@@ -1,22 +1,32 @@
-const Cards = ({props}) => {
+const Cards = ({props, removeTask}) => {
   
   const {task, id} = props // desconstruindo array
 
 
-  function clickButton() { 
-
+  function deleteTask() { 
     // Function DELETE task
+    console.log('Deleção Acionada', id)
 
-    console.log('Botão Pressionado', id)
+    console.log(removeTask(id))
     
+    removeTask(id)
+
+  }
+
+  function updateTask() { 
+    // Function UPDATE task
+    console.log('Atualização Acionada', id)
+
+    utualizaTask(id) //  utualizaTask is not defined ??
+
   }
   
   return (
     <div className="flex flex-row bg-[#777] w-[550px] h-[100px] rounded-[5px] justify-between overflow-hidden border-[3px] border-[#333]">
       <h2 className="grow ml-[30px] my-auto text-2xl font-semibold text-white">{task}</h2>
       <div className="flex flex-col border-l-[3px] border-[#333]">
-        <button onClick={ () => clickButton()} className="w-[52px] h-[50%] bg-[#4cff38] cursor-pointer border-b-[1.5px] border-[#333] hover:bg-[#278419]">✅</button>
-        <button className="w-[52px] h-[50%] bg-[#ffe53b] cursor-pointer border-t-[1.5px] border-[#333] hover:bg-[#a89726]">🔄</button>
+        <button onClick={() => deleteTask()} className="w-[52px] h-[50%] bg-[#4cff38] cursor-pointer border-b-[1.5px] border-[#333] hover:bg-[#278419]">✅</button>
+        <button onClick={() => updateTask()} className="w-[52px] h-[50%] bg-[#ffe53b] cursor-pointer border-t-[1.5px] border-[#333] hover:bg-[#a89726]">🔄</button>
       </div>
     </div>
   );
--- app/src/components/CardsGroup.jsx
+++ app/src/components/CardsGroup.jsx
@@ -2,26 +2,42 @@ import Card from './Card'
 
 const CardsGroup = (props) => {
 
-  const {task} = props 
+  const { task, setTask } = props 
 
+  function removeTask(id) {
+    const updateTasks = task.filter(item => item.id !== id)
+    return updateTasks
+    
+    //ERROR
+  }
 
+  function utualizaTask(id) {
+    const updateTasks = task.find(item => item.id === id)
+    return updateTasks
+    
+    //ERROR
+  }
 
+  // TODO: Adicionar função ativada ao pressionar o botão "✅" para completar (deletar) -- CHECH
+  // TODO: Adicionar função ativada ao pressionar o botão "🔄" para dar update -- CHECK
+  // TODO: Passar ambas funções anteriores para o componente <Card /> abaixo -- CHECK
 
- 
 
   // TODO: Acessar dados de tarefas já gerada
 
-  // TODO: Adicionar função ativada ao pressionar o botão "✅" para completar (deletar)
-  // TODO: Adicionar função ativada ao pressionar o botão "🔄" para dar update
-
-  // TODO: Passar ambas funções anteriores para o componente <Card /> abaixo
 
   // TODO: Adicionar mapeamento à partir de estado para gerar visualização dos dados na tela
 
   return (
     <div className="flex flex-col gap-[15px] items-center w-full min-h-[screen] mb-[42px]">
 
-      {task && task.map(element => <Card props={element} key={element.id} /> )}
+      {task && task.map(element => 
+        <Card 
+          props={element}  
+          key={element.id} 
+          removeTask={removeTask} 
+          utualizaTask={utualizaTask}
+        /> )}
 
     </div>
   );
