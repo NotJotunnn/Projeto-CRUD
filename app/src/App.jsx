b1ac366d22f815194b696bb146ed7938406f03ca
João Gabriel <jota.amaralmiranda@gmail.com>
2024-04-11 19:33:54 -0300

    Adding create task (Incomplete)

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,22 +4,16 @@ import TaskForm from "./components/TaskForm"
 
 function App() {
 
-  // * Adicionar estado para guardar os dados das tarefas
-  // * Passar estado dos dados das tarefas para <CardsGroup />
   // TODO: Acessar os dados do banco
 
-  const [task, setTask] = useState([{"id":4,"task":"cleanupAvenue"},{"id":5,"task":"cleanupAvenue"},{"id":"182ea6b9-19c0-4a42-b435-55c6ce501eb5","task":"takeTheGarbageOut"}]);
+  const [task, setTask] = useState([{"id":6,"task":"Sla"},{"id":4,"task":"cleanupAvenue"},{"id":5,"task":"cleanupAvenue"},{"id":"182ea6b9-19c0-4a42-b435-55c6ce501eb5","task":"takeTheGarbageOut"}]);
 
   const props = {task, setTask};
-
-
-  // TODO: Passar props para <TaskForm />
-
 
   return (
     <div className="flex flex-col gap-5 items-center w-full min-h-screen bg-[#eee]">
       <h1 className="text-[#222] text-center text-7xl font-semibold mt-[42px]">Tarefas!</h1>
-      <TaskForm />
+      <TaskForm props={props} />
       <CardsGroup props={props}/>
     </div>
   )
--- app/src/components/TaskForm.jsx
+++ app/src/components/TaskForm.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
-const TaskForm = () => {
+const TaskForm = (props) => {
+
+  const { task, setTask } = props 
 
   // TODO: Acessar props enviados e desconstruir dele a função para alterar as tasks
 
@@ -12,13 +14,24 @@ const TaskForm = () => {
   ]
 
   const [inputValue, setInputValue] = useState()
-  const [currentPlaceholder, setCurrentPlaceholder] = useState(placeholderList[0])
-  
+  const [currentPlaceholder, setCurrentPlaceholder] = useState(placeholderList[Math.floor(Math.random() * placeholderList.length)])
+  // !const [newTask] = useState() ??
 
   function inputChange(e) {
     setInputValue(e.target.value)
-    setCurrentPlaceholder(placeholderList[Math.floor(Math.random() * (placeholderList.length))])
+    setCurrentPlaceholder()
+    
+  
   }
+
+  function sendTaskData() {
+    console.log(inputValue)
+    if (inputValue != undefined ) {
+      newTask(inputValue)  
+    }
+
+  }
+
 
   // TODO: Criar uma função onclick para enviar os dados para o banco E à variável task (usar setTask)
   // TODO: Adicionar validação de dados para prevenir enviar uma tarefa vazia
@@ -26,7 +39,7 @@ const TaskForm = () => {
   return (
     <div className="flex flex-row justify-center gap-[15px] items-center w-full h-[250px]">
         <input onChange={inputChange} className="p-[12px] min-w-[450px] h-[55px] bg-white border-[3px] text-[#333] border-[#333] rounded-[5px]" type="text" placeholder={currentPlaceholder} value={inputValue}/>
-        <button className="text-black bg-[#4cff38] hover:bg-[#278419] hover:text-white h-[55px] py-[12px] px-[17px] rounded-[5px] font-semibold border-[3px] border-[#333]">Adicionar</button>
+        <button onClick={sendTaskData} className="text-black bg-[#4cff38] hover:bg-[#278419] hover:text-white h-[55px] py-[12px] px-[17px] rounded-[5px] font-semibold border-[3px] border-[#333]">Adicionar</button>
     </div>
   );
 }
